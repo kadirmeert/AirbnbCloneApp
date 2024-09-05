e3775641ece14d4eea59f1d3997901f73511375a
kadir mert yildiz <kadirmert.34@hotmail.com>
2024-09-05 21:53:12 +1000

    Initial Commit

---
+++ AirbnbCloneApp/AirbnbCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneAppApp.swift
+//  AirbnbCloneApp
+//
+//  Created by Kadir Yildiz on 5/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
